1636820d5f4658ebdcd47adf4501deac1d9a5abe
Benjamin Legrand <dev@legrand.ws>
2015-04-28 22:39:27 +0200

    [liblight] Add liblight,
    We only receive notification request when the usb cable is plugged.
    This should be investigated a little bit more.
    This requires commit 867108085fe1c69c1d5bfe23dca3dbe243b7fcbe in the
    kernel
    
    Change-Id: Ifa56c13b4a45b89c6cd987abf296fbeebeba781e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -106,6 +106,10 @@ TARGET_LIBINIT_DEFINES_FILE := $(LOCAL_PATH)/init/init_surnia.c
 # Asserts
 TARGET_OTA_ASSERT_DEVICE := XT1521,XT1524,XT1526,XT1527,surnia_uds,surnia_umts,surnia
 
+
+# Lights
+TARGET_PROVIDES_LIBLIGHT := true
+
 # Media
 TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
 
